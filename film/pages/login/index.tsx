0b531be85e495794547900b7526a594348ae957c
Fredrik Carsten Hansteen <fredrik.c.hansteen@ntnu.no>
2024-02-06 12:29:32 +0100

    Resolve "Create navbar"

---
+++ film/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RootLayout from "@/app/layout";
+
+const LoginPage: React.FC = () => {
+  return (
+    <RootLayout>
+      <div className="flex-grid">
+        <h1>Login Page</h1>
+      </div>
+    </RootLayout>
+  );
+};
+
+export default LoginPage;
