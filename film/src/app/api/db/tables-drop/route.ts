f4bbfcb2e774ad5cf22779e3c4fc31ba6fa5fd4a
Jonas Elvedal Hole <jonas.e.hole@ntnu.no>
2024-02-15 13:12:10 +0100

    Resolve "Create a Schema for the DB"

--- film/src/app/api/db/tables-drop/route.ts
+++ film/src/app/api/db/tables-drop/route.ts
@@ -1,9 +1,12 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store'
+
 export async function GET(request: Request) {
   try {
-    const result = await sql`DROP TABLE person;`;
+    const result = await sql`DROP TABLE folk, likes, watched;`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
