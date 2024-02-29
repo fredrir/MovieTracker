d9050dd8eef06a45c52badcb6f5765ba2b3e7f2b
reginehn <reginehn@stud.ntnu.no>
2024-02-29 11:34:15 +0100

    made easy button

--- film/src/app/random/page.tsx
+++ film/src/app/random/page.tsx
@@ -4,12 +4,25 @@ import { useEffect, useState } from "react";
 import { GET } from "../api/movie-details/route";
 import { MovieDetails } from "@/models/movieDetails";
 import { useRouter } from "next/navigation";
+import {render} from 'react-dom'
+import { Button } from "@mui/material";
 
-export default function HomePage() {
+export default function RandomPage() {
+    const randomInt = (min:number, max:number) =>
+        Math.floor(Math.random() * (max - min + 1)) + min;
+    const randomNumber: number= randomInt(100000,900000);
+    const randomUrl=`/${randomNumber}` //url that redirect to the overview of random movie
+    const router = useRouter();
+    const handleButtonClick = () => {
+        router.push(randomUrl);
+    }
 
     return (
         <>
         <h1>Random movie</h1>
+            <Button onClick={handleButtonClick}>
+                Get a random movie
+            </Button>
         </>
     )
 }
