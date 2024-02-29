bd2288b6440632553a08c6fd0961bd115b927a32
reginehn <reginehn@stud.ntnu.no>
2024-02-29 13:11:03 +0100

    two suggestions for format

--- film/src/app/random/page.tsx
+++ film/src/app/random/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import FilmCard from "@/components/FilmCard/FilmCard";
-import { useEffect, useState } from "react";
-import { GET } from "../api/movie-details/route";
+import { MovieOverviewCard } from "@/components/MovieOverviewCard/MovieOverviewCard";
 import { MovieDetails } from "@/models/movieDetails";
 import { useRouter } from "next/navigation";
-import {render} from 'react-dom'
-import { Button } from "@mui/material";
+import { useState } from "react";
 
 export default function RandomPage() {
+    const [movieDetails, setMovieDetails] = useState<MovieDetails>();
     const randomInt = (min:number, max:number) =>
         Math.floor(Math.random() * (max - min + 1)) + min;
     const randomNumber: number= randomInt(100000,900000);
@@ -16,13 +14,27 @@ export default function RandomPage() {
     const handleButtonClick = () => {
         router.push(randomUrl);
     }
+    
 
     return (
         <>
+        <div className="justify-center flex">
         <h1>Random movie</h1>
+
+            {/* FORSLAG 1:
             <Button onClick={handleButtonClick}>
                 Get a random movie
-            </Button>
+            </Button>*/}
+
+
+            {/* FORSLAG 2:
+            <MovieOverviewCard 
+            movie_title={movieDetails?.title}
+            movie_image={movieDetails?.backdrop_path}
+            movie_description={movieDetails?.overview}
+            movie_id={movieDetails?.id}
+            />*/}
+        </div>
         </>
     )
 }
