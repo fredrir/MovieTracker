132a866c7536d64697585b839e06d9835d225de5
fredrir <fhansteen@gmail.com>
2024-02-29 11:38:46 +0100

    better ui in recommendations

--- film/src/app/recommendations/page.tsx
+++ film/src/app/recommendations/page.tsx
@@ -1,45 +1,52 @@
 "use client";
-import { Movie } from "@/models/movie";
-import { getRecomendedGenres } from "@/utils/recommendedGenres";
-import { useEffect, useState } from "react";
-import { GET } from "../api/movie-recommendations/route";
 
-export default function Recomendations() {
+import { useEffect, useState } from "react";
+import FilmCard from "@/components/FilmCard/FilmCard";
+import { getRecomendedGenres } from "@/utils/recommendedGenres";
+import { Movie } from "@/models/movie";
+import { useRouter } from "next/navigation";
+
+export default function Recommendations() {
+  const router = useRouter();
   const [movies, setMovies] = useState<Movie[]>([]);
+
   useEffect(() => {
     async function getNewMovies() {
       try {
-        const userId = localStorage.getItem("userId" || "1");
+        const userId = localStorage.getItem("userId") || "1";
         if (!userId) {
           console.error("User ID not found in cookies");
           return;
         }
-        if (userId) {
-          console.log("User ID found in cookies and is " + userId);
-        }
         const genres: number[] = await getRecomendedGenres(userId);
-        console.log("genres" + genres);
         const url = `/api/movie-recommendations?genre=${genres.join(",")}`;
         const request = new Request(url);
-        const response = await GET(request);
+        const response = await fetch(request);
         const data = await response.json();
-        console.log("fetched data" + (await JSON.stringify(data)));
         setMovies(data.data.results);
-      } catch (error) {}
+      } catch (error) {
+        console.error("Failed to fetch new movies", error);
+      }
     }
     getNewMovies();
   }, []);
 
   return (
     <div>
-      <h1>Recommendations</h1>
-      {movies?.map((movie, index) => {
-        return (
-          <div key={index}>
-            <h1>{movie.title}</h1>
-          </div>
-        );
-      })}
+      <h1 className="text-center text-4xl">Recommendations</h1>
+      <div className="grid grid-cols-6 gap-2">
+        {movies.map((movie) => (
+          <FilmCard
+            key={movie.id}
+            title={movie.title}
+            image={"https://image.tmdb.org/t/p/original/" + movie.backdrop_path}
+            description=""
+            onClick={() => {
+              router.push(`/${movie.id}`);
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
